1343a7635f32c84cf8495ca5d18e1893bd5ffacd
joms-z <joms-z@users.noreply.github.com>
2015-04-14 00:48:35 -0400

    Update feature_handling.py
    Added new function avg_feature to compute x, y coordinates as new features.
    (There is lot of code duplication. Deal with this later)

--- preprocess/feature_handling.py
+++ preprocess/feature_handling.py
@@ -12,6 +12,17 @@ def avg(ndimage, label_dict, group_number):
         feature_avg[label] += ndimage[x]
     feature_avg = feature_avg/total
     return feature_avg
+    
+def avg_feature(ndimage, label_dict, group_number):
+    imagedim = (ndimage.shape[0], ndimage.shape[1])
+    feature_avg = zeros((group_number, (ndimage.shape[2] + 2))) #(15 features)
+    total = zeros((group_number, 1))
+    for x in ndindex(imagedim):
+        label = label_dict[x]
+        total[label] += 1
+        feature_avg[label] += append(ndimage[x], [x[0]+1, x[1]+1]) #added x and y coordinates (location)
+    feature_avg = feature_avg/total
+    return feature_avg
 
 
 def superpixel_target(target, group_number):
